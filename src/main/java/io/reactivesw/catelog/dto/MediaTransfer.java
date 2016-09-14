b91f5adc78b4bf900610931c1b74ca286269373f
Davis <davis.dai@go6d.com>
2016-09-14 12:08:56 +0800

    change to root.

---
+++ src/main/java/io/reactivesw/catelog/dto/MediaTransfer.java
@@ -0,0 +1,36 @@
+package io.reactivesw.catelog.dto;
+
+import io.reactivesw.catelog.domain.Media;
+import io.reactivesw.catelog.infrastructure.MediaInfo;
+
+/**
+ * this is transfer class for Media.
+ * 
+ * @author Davis
+ *
+ */
+public final class MediaTransfer {
+  
+  /**
+   * private constructor.
+   */
+  private MediaTransfer(){}
+
+  /**
+   * transfer Media to MediaInfo.
+   * 
+   * @param media media
+   * @return MediaInfo
+   */
+  public static MediaInfo transferToMediaInfo(Media media) {
+    final MediaInfo.Builder builder = MediaInfo.newBuilder();
+
+    builder.setId(media.getId());
+    builder.setType(media.getType());
+    builder.setTitle(media.getTitle());
+    builder.setUrl(media.getUrl());
+    builder.setDisplayOrder(media.getDisplayOrder());
+
+    return builder.build();
+  }
+}
