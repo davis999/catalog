2234ce4b63b5a243aa246af896cab18cec18a8b4
Davis <davis.dai@go6d.com>
2016-09-20 14:24:01 +0800

    change catelog to catalog.

---
+++ src/main/java/io/reactivesw/catalog/exception/CatalogRuntimeException.java
@@ -0,0 +1,16 @@
+package io.reactivesw.catalog.exception;
+
+/**
+ * catalog runtime exception.
+ * 
+ * @author Davis
+ *
+ */
+public class CatalogRuntimeException extends RuntimeException {
+
+  /**
+   * auto generated serial id.
+   */
+  private static final long serialVersionUID = 6797042230823725800L;
+
+}
