bb69755a10743a2cbd66482de460e96bdcdfc284
Davis <davis.dai@go6d.com>
2016-10-14 13:53:24 +0800

    test auto deply from travis to k8s

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,11 @@
 #! /bin/sh
 
-echo "hello world"
+#删除rc
+curl -X DELETE http://54.200.161.225:8080/api/v1/namespaces/default/replicationcontrollers/cust-catalog
+
+#删除pod
+curl -X DELETE http://54.200.161.225:8080/api/v1/namespaces/default/pods/cust-catalog-tvhcy
+
+#提交json，新建rc
+curl -X POST http://54.200.161.225:8080/api/v1/namespaces/default/replicationcontrollers \
+	  -H "Content-Type:application/yaml" --data-binary "@cust-catalog-rc.yaml"
