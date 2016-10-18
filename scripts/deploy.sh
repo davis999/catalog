b04913b1a22e6daf9cf0e5239fa29b516033f91a
Davis <davis.dai@go6d.com>
2016-10-18 09:45:25 +0800

    remove tomcat server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,28 @@
 #! /bin/sh
 
+#设置名字
+serviceName="cust-catalog"
+readonly serviceName
+
 #删除rc
-echo "\n----------------------------------start delete cust-catalog rc-----------------------------------------------\n"
-curl -X DELETE http://54.200.161.225:8080/api/v1/namespaces/default/replicationcontrollers/cust-catalog
-echo "\n----------------------------------end delete cust-catalog rc-------------------------------------------------\n"
+echo "\n----------------------------------start delete ${serviceName} rc---------------------------------------------\n"
+curl -X DELETE http://54.200.161.225:8080/api/v1/namespaces/default/replicationcontrollers/${serviceName}
+echo "\n----------------------------------end delete ${serviceName} rc-----------------------------------------------\n"
 
 #获取到所有的pods
-echo "\n----------------------------------start get all cust-catalog pod---------------------------------------------\n"
+echo "\n----------------------------------start get all ${serviceName} pod-------------------------------------------\n"
 podList=$(curl -X GET http://54.200.161.225:8080/api/v1/namespaces/default/pods)
-echo "\n----------------------------------end get all cust-catalog pod-----------------------------------------------\n"
+echo "\n----------------------------------end get all ${serviceName} pod---------------------------------------------\n"
 
-#删除所有cust-catalog的pod
-echo "\n----------------------------------start delete all cust-catalog pod------------------------------------------\n"
-for podName in $(echo $podList | grep -oE 'cust-catalog-[a-zA-Z0-9]{1,5}')
+#删除所有pod
+echo "\n----------------------------------start delete all ${serviceName} pod----------------------------------------\n"
+for podName in $(echo $podList | grep -oE ${serviceName}"-[a-zA-Z0-9]{1,5}")
 do
 	curl -X DELETE http://54.200.161.225:8080/api/v1/namespaces/default/pods/$podName
 done
-echo "\n----------------------------------end delete all cust-catalog pod--------------------------------------------\n"
+echo "\n----------------------------------end delete all ${serviceName} pod------------------------------------------\n"
 
-#提交json，新建rc
+#提交yaml文件，新建rc
 echo "------------------------------------start post cust-catalog-rc.yaml to k8s-------------------------------------\n"
 curl -X POST http://54.200.161.225:8080/api/v1/namespaces/default/replicationcontrollers \
 	  -H "Content-Type:application/yaml" --data-binary "@./scripts/cust-catalog-rc.yaml"
