bf67a1a465af6d257c26d76402b943f588ade0da
kaka <liuquan89@gmail.com>
2016-10-27 17:03:51 +0800

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,24 +6,24 @@ readonly serviceName
 
 #删除rc
 echo "\n----------------------------------start delete ${serviceName} rc---------------------------------------------\n"
-curl -X DELETE http://54.200.161.225:8080/api/v1/namespaces/default/replicationcontrollers/${serviceName}
+curl -X DELETE https://35.161.132.35:8080/api/v1/namespaces/default/replicationcontrollers/${serviceName}
 echo "\n----------------------------------end delete ${serviceName} rc-----------------------------------------------\n"
 
 #获取到所有的pods
 echo "\n----------------------------------start get all ${serviceName} pod-------------------------------------------\n"
-podList=$(curl -X GET http://54.200.161.225:8080/api/v1/namespaces/default/pods)
+podList=$(curl -X GET https://35.161.132.35:8080/api/v1/namespaces/default/pods)
 echo "\n----------------------------------end get all ${serviceName} pod---------------------------------------------\n"
 
 #删除所有pod
 echo "\n----------------------------------start delete all ${serviceName} pod----------------------------------------\n"
 for podName in $(echo $podList | grep -oE ${serviceName}"-[a-zA-Z0-9]{1,5}")
 do
-	curl -X DELETE http://54.200.161.225:8080/api/v1/namespaces/default/pods/$podName
+	curl -X DELETE https://35.161.132.35:8080/api/v1/namespaces/default/pods/$podName
 done
 echo "\n----------------------------------end delete all ${serviceName} pod------------------------------------------\n"
 
 #提交yaml文件，新建rc
 echo "------------------------------------start post cust-catalog-rc.yaml to k8s-------------------------------------\n"
-curl -X POST http://54.200.161.225:8080/api/v1/namespaces/default/replicationcontrollers \
+curl -X POST https://35.161.132.35:8080/api/v1/namespaces/default/replicationcontrollers \
 	  -H "Content-Type:application/yaml" --data-binary "@./scripts/cust-catalog-rc.yaml"
 echo "\n----------------------------------end post cust-catalog-rc.yaml to k8s---------------------------------------\n"
